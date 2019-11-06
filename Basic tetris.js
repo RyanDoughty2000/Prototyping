f87de1cf327a16e3344794851fe23db09f364ef6
RyanDoughty2000 <57438996+RyanDoughty2000@users.noreply.github.com>
2019-11-06 10:10:08 +0000

    Add files via upload

---
+++ Basic tetris.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html lang ='en'>
+
+    <head>
+    meta charset = 'UTF-8' >
+    <style>
+    canvas {
+        position:absolute;
+        top: 45 % ;
+        left: 50 % ;
+        width: 640 px;
+        height: 640 px;
+        margin: -320 px 0 0 - 320 px;
+    } 
+    </style>
+    </head>
+     <body>
+         <canvas>
+             <script>
+                 
+             </script>
+         </canvas>
+     </body>
